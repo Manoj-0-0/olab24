255daec6248f9f70603b5885946c31ace00b7c7c
Manoj-0-0 <ohbhaimaromujhe100@gmail.com>
2024-11-03 23:29:26 +0530

    Done

--- DOP/src/newshelf/IBook.java
+++ DOP/src/newshelf/IBook.java
@@ -1,5 +1,28 @@
-package newshelf;
 
-public interface IBook {
+package newshelf;
+public sealed interface IBook permits Comic,TextBook,Fiction{
+
+}
+
+enum FictionType {
+
+	Comedy,
+	Tragedy,
+	GoK
+}
+
+record Comic(String Title,int ageOfMainCharacter) implements IBook{
+	
+
+
+}
+
+record Fiction(String name,FictionType type) implements IBook{
+
+
+}
+
+record TextBook(String subject) implements IBook {
+
 
 }
